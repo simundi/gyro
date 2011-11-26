a9d3d77103fbf5720307eb9a4af8216a81a9b5eb
Alexandre Simundi <simundi@gmail.com>
2011-11-26 16:25:55 +0000

    Intial strucutre of scala-tour

---
+++ scala-tour/src/test/scala/com/simundi/scalatour/level/a1/OperatorTest.scala
@@ -0,0 +1,13 @@
+package com.simundi.scalatour.level.a1
+import org.scalatest.FunSuite
+
+class CalculatorTest extends FunSuite{
+
+
+  test("Test sum opperator"){
+    val sum = 1 + 1
+    assert (sum == 2)
+    
+  }
+
+}
